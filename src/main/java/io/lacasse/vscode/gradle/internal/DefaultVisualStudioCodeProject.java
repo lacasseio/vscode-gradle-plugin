dd23926f719e93c0091254cfe8c9997176ca0660
Daniel Lacasse <daniel@lacasse.io>
2018-10-11 09:07:39 +0200

    Revert "Caveman debugging"
    This reverts commit 67c6585a6eb6f7b2b0c736d0589ccaf99c9da5c5.

--- src/main/java/io/lacasse/vscode/gradle/internal/DefaultVisualStudioCodeProject.java
+++ src/main/java/io/lacasse/vscode/gradle/internal/DefaultVisualStudioCodeProject.java
@@ -142,8 +142,6 @@ public class DefaultVisualStudioCodeProject implements VisualStudioCodeProject {
                 return result;
             }));
 
-            System.out.println("NAME : " + project.getName());
-            System.out.println(GenerateCompileCommandsFileTask.taskName(binary));
             it.getCompileCommandsLocation().set(generateCompileCommandsFileFor(project.getTasks(), binary));
         };
     }
